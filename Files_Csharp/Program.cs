7e130150da0652660d7384e8dcdc4b1b4942ad5c
Davidson Dias <davidsonsd16@gmail.com>
2019-10-31 23:34:16 -0300

    Files

---
+++ Files_Csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace Files_Csharp {
+    class Program {
+        static void Main(string[] args) {
+
+            string pathIn = @"C:\Users\david\Downloads\Curso - POO Csharp\Code\Files_Csharp\data.csv";
+            string pathOut = @"C:\Users\david\Downloads\Curso - POO Csharp\Code\Files_Csharp\out\summary.csv";
+            //Directory.CreateDirectory(@"C:\Users\david\Downloads\Curso - POO Csharp\Code\Files_Csharp\out");
+            try {
+                string[] lines = File.ReadAllLines(pathIn);
+
+                using (StreamWriter sw = File.AppendText(pathOut)) {
+                    foreach (string line in lines) {
+                        string[] columns = line.Split(",");
+                        double Total = double.Parse(columns[1], CultureInfo.InvariantCulture) * int.Parse(columns[2], CultureInfo.InvariantCulture);
+                        sw.WriteLine(columns[0] + "," + Total.ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch(IOException e) {
+                Console.WriteLine("Unexpectep error: " + e.Message);
+            }
+        }
+    }
+}
